aa933ff0d3cbf04178a9262bbb33b5e78d2f49e8
Claire Winogrodzki <92890574+cwinogrodzki@users.noreply.github.com>
2025-02-19 00:02:09 -0500

    Update main.c

--- blinky/main.c
+++ blinky/main.c
@@ -1,38 +1,25 @@
 #include <stdint.h>
 
-#define PERIPHERAL_BASE (0x40000000U)
-#define AHB1_BASE (PERIPHERAL_BASE + 0x20000U)
-#define AHB2_BASE (PERIPHERAL_BASE + 0x8000000U)
-#define GPIOA_BASE (AHB2_BASE + 0x0U)
-#define RCC_BASE (AHB1_BASE + 0x1000U)
-
-#define RCC_AHBENR_OFFSET (0x14U)
-#define RCC_AHBENR ((volatile uint32_t*) (RCC_BASE + RCC_AHBENR_OFFSET))
-#define RCC_AHBENR_GPIOAEN (0x00U)
-
-#define GPIO_MODER_OFFSET (0x00U)
-#define GPIOA_MODER ((volatile uint32_t*) (GPIOA_BASE + GPIO_MODER_OFFSET))
-#define GPIO_MODER_MODER5 (10U)
-#define GPIO_ODR_OFFSET (0x14U)
-#define GPIOA_ODR ((volatile uint32_t*) (GPIOA_BASE + GPIO_ODR_OFFSET))
-
-#define LED_PIN 5
-
-void main(void)
+int main(void)
 {
-  *RCC_AHBENR |= (1 << RCC_AHBENR_GPIOAEN);
+	uint32_t *pClockControlReg = (uint32_t*)0x40021014;
+	uint32_t *pGPIOAModeReg = (uint32_t*)0x48000000;
+	uint32_t *pGPIODOutReg = (uint32_t*)0x48000014;
 
-  // do two dummy reads after enabling the peripheral clock, as per the errata
-  volatile uint32_t dummy;
-  dummy = *(RCC_AHBENR);
-  dummy = *(RCC_AHBENR);
+    //activate clock for GPIOA peripheral in AHBNENR
+	*pClockControlReg |= (1 << 17); //set bit 17 to "1"
 
-  *GPIOA_MODER |= (1 << GPIO_MODER_MODER5);
+	//set GPIOA pin mode to output
+	*pGPIOAModeReg &= ~(3 << 10); //clear the 10th and 11th bits
+	*pGPIOAModeReg |= (1 << 10);  //set 10th bit to "1"
 
-  while(1)
-  {
-    *GPIOA_ODR ^= (1 << LED_PIN);
-    for (uint32_t i = 0; i < 1000000; i++);
-  }
+	//set pin 5 to high (turn on led)
+	//*pGPIODOutReg |= (1 << 5); //set bit 5 to "1"
 
+	//toggle the LED
+	while(1)
+	{
+		*pGPIODOutReg ^= (1 << 5);
+		 for (uint32_t i = 0; i < 1000000; i++);
+	}
 }
