ee6fadec8ab0bf6584866f59c7f49a2d55fbbc83
cwinogrodzki <clairewinogrodzki@gmail.com>
2025-09-01 14:51:57 -0400

    button driver

--- bsp/inc/bsp.h
+++ bsp/inc/bsp.h
@@ -13,10 +13,15 @@
 /*
  * LED driver functions
  */
-
 void	BSP_LED_Init	(void);
 void	BSP_LED_On	(void);
 void	BSP_LED_Off	(void);
 void	BSP_LED_Toggle	(void);
+
+/*
+ * Push-Button driver functions
+ */
+void       BSP_PB_Init		(void);
+uint8_t    BSP_PB_GetState	(void);
 
 #endif /* INC_BSP_H_ */
